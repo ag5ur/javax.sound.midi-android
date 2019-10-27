0416ae436f28672361d0fbba3d25a507be169d21
ag5ur <57003562+ag5ur@users.noreply.github.com>
2019-10-27 04:15:19 -0500

    bump version to 0.0.4-alpha

--- javax.sound.midi/src/main/java/javax/sound/midi/impl/SequencerImpl.java
+++ javax.sound.midi/src/main/java/javax/sound/midi/impl/SequencerImpl.java
@@ -515,7 +515,7 @@ public class SequencerImpl implements Sequencer {
     @NonNull
     @Override
     public Info getDeviceInfo() {
-        return new Info("Sequencer", "javax.sound.midi", "Android MIDI Sequencer", "master-SNAPSHOT");
+        return new Info("Sequencer", "javax.sound.midi", "Android MIDI Sequencer", "0.0.4-alpha");
     }
 
     @Override
